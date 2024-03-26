32cca1ccc21a7a31f3bdf3a7f13d558c34080b89
Gladstone Rose <gladstonerose@hotmail.com>
2024-03-25 20:45:45 -0500

    modify files

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 // This file can include any JavaScript you need for interactivity
 // For the current setup, no JavaScript is needed as the interaction is handled by links
+// Get the button
+var backToTopBtn = document.getElementById("backToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+backToTopBtn.onclick = function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+};
